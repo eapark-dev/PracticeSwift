ec335a9ce25aecec055bb1479d132a123f56213d
eapark-dev <eapark5908@gmail.com>
2022-01-07 21:03:48 +0900

    swift 언어 공부 - 스톱워치 만들기 UI 완료

---
+++ Tutorial1/Tutorial1/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Tutorial1
+//
+//  Created by 박은아 on 2022/01/07.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
---
+++ Tutorial1/Tutorial1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tutorial1
+//
+//  Created by 박은아 on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
