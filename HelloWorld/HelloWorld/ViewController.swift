45147206c09bb8f9ac5a1a9cd909454fca4a94b1
eapark-dev <eapark5908@gmail.com>
2022-01-13 21:53:23 +0900

    HelloWorld 찍기 - Ios 개발 공부, iOS target 13.0 오류 해결

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 박은아 on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
