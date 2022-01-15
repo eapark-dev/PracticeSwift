b1f6ef472a474df3b47e82d2d3ffd4d508f7ecf4
eapark-dev <eapark5908@gmail.com>
2022-01-15 21:33:19 +0900

    네비게이션 - 클릭 이벤트

---
+++ HelloWorld/HelloWorld/DetailController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailController.swift
+//  HelloWorld
+//
+//  Created by 박은아 on 2022/01/15.
+//
+
+import UIKit
+
+class DetailController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func Click_moveBtn(_ sender: Any) {
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "DetailController"){
+            self.navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
