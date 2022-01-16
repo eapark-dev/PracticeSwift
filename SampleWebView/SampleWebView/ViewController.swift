c9e0f1138eff22b60f7f9df83dfcfada3e38b7c7
eapark-dev <eapark5908@gmail.com>
2022-01-16 19:52:40 +0900

    swift - 옵셔널 바인딩 공부

---
+++ SampleWebView/SampleWebView/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SampleWebView
+//
+//  Created by 박은아 on 2022/01/16.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var WebViewMain: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //1. url string
+        //2. url > request
+        //3. req > load
+        
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString) { //unwrap 과정 --옵셔널 바인딩
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
+    }
+
+
+}
+
