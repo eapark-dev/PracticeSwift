a68c593436d254c1601c5ec9583c71e5f7f85b16
eapark-dev <eapark5908@gmail.com>
2022-01-25 20:06:47 +0900

    ios 공부 - stack , pop 이용하여 로그인 화면과 회원가입 화면

---
+++ NavigationViewController_tutorial/NavigationViewController_tutorial/Controllers/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NavigationViewController_tutorial
+//
+//  Created by 박은아 on 2022/01/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //상단 네비게이션 바 부분을 숨김 처리한다.
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+
+}
+
