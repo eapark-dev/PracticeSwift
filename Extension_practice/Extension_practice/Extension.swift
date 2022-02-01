4ae4f22f12c7cd5f899f47e03eac9e5702e8dd80
eapark-dev <eapark5908@gmail.com>
2022-02-01 18:25:00 +0900

    swift - extension 사용법 (공통적으로 사용할 메소드를 정의할 때 사용)

---
+++ Extension_practice/Extension_practice/Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Extension.swift
+//  Extension_practice
+//
+//  Created by 박은아 on 2022/02/01.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func sayHi() {
+        print("Hi")
+    }
+    
+}
---
+++ Extension_practice/Extension_practice/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Extension_practice
+//
+//  Created by 박은아 on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //extension을 사용할 경우 공통적인 부분을 메소드로 정의해서 사용할 수 있다.
+        sayHi()
+
+    }
+
+
+}
+
