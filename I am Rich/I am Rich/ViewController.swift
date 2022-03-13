c1970d52d08abf552cd143b15c35f7677472c956
eapark-dev <eapark5908@gmail.com>
2022-03-13 17:18:13 +0900

    유데미 - iOS 강의 시작,

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 박은아 on 2022/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
