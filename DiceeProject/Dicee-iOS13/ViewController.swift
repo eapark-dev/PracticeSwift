c42e361087d29ad7a9bc54a4b2931e7dabd9823e
eapark-dev <eapark5908@gmail.com>
2022-03-17 22:14:22 +0900

    iOS 공부 - image 버튼 ViewController에 연결하여 이미지 변경하기

--- DiceeProject/Dicee-iOS13/ViewController.swift
+++ DiceeProject/Dicee-iOS13/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Who.What = Value => 누가 변경해야 하나.
+        //#imageLiteral( ->이미지리터럴 xcode13부터 안될 때 사용법
+        //WHO          WHAT    VALUE
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
     }
 
 
