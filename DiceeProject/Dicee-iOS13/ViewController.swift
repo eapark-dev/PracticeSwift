9d83e7750e1cad693929379ef17b51f2d07020b4
eapark-dev <eapark5908@gmail.com>
2022-03-19 21:08:59 +0900

    주사위 게임 - 버튼 클릭 시 값 주사위 값 변경 , MyPlayground 파일 삭제

--- DiceeProject/Dicee-iOS13/ViewController.swift
+++ DiceeProject/Dicee-iOS13/ViewController.swift
@@ -11,25 +11,37 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //주사위 숫자 초기화
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Who.What = Value => 누가 변경해야 하나.
-        //#imageLiteral( ->이미지리터럴 xcode13부터 안될 때 사용법
-        //WHO          WHAT    VALUE
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("주사위 버튼 변경")
+        print("call rollButtonPressed")
+        //대괄호 컬렉션을 보유하는 데 사용
+        diceImageView1.image =  [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image =  [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        
+        
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber = rightDiceNumber - 1
+        
+        if leftDiceNumber == 5
+        {
+            leftDiceNumber = 0
+        }
+        
+        if rightDiceNumber == 1
+        {
+            rightDiceNumber = 5
+        }
     }
     
 }
