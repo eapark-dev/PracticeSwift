2daa22be61ce12ac6a5774ddabe8155ae945fb7b
eapark-dev <eapark5908@gmail.com>
2022-03-29 22:09:15 +0900

    iOS 공부 - 타이머 설정

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -9,16 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     //초기화
-    let eggTimes = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    let eggTimes = ["Soft" : 3, "Medium" : 4, "Hard" : 5]
     
+    var seconds = 60
+    var progressTime = 100
+    var timer = Timer()
+    
+    @IBOutlet weak var timerBar: UIProgressView!
+    @IBOutlet weak var timerText: UILabel!
     @IBAction func HardnessSelected(_ sender: UIButton){
         
+        timerBar.progress = 1.0
+        timer.invalidate() //초기화
+        timerBar.tag = 1
         let hardness = sender.currentTitle!
-   
-        print(eggTimes[hardness]!)
+        
+        seconds = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
         
     }
     
+    @objc func updateTime()
+    {
+        if(seconds > 0)
+        {
+            seconds -= 1
+        //    timerBar.setProgress(<#T##progress: Float##Float#>, animated: <#T##Bool#>)
+        }else{
+            timer.invalidate()
+            timerText.text = "완료"
+        } 
+        
+        print("\(seconds) seconds.")
+    }
 }
